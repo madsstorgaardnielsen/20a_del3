78351c191d03cf98345501667e0492e78013a948
Mads Storgaard-Nielsen <s180076@student.dtu.dk>
2019-11-29 12:06:25 +0100

    added main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import controller.GameLogic;
+import model.LanguageManager;
+
 public class Main {
     public static void main(String[] args) {
+        LanguageManager language = new LanguageManager();
+        language.selectLanguage();
 
+        GameLogic game = new GameLogic();
+        game.runGame();
     }
 }
